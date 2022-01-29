15f781af7e3454d6c806b47135885a1160cfbfa5
Matthijs S. Berends <m.s.berends@umcg.nl>
2022-01-29 15:02:55 +0100

    centre_mean(), ci(), scale_sd()

--- R/distribution_metrics.R
+++ R/distribution_metrics.R
@@ -23,13 +23,13 @@
 #' @param x values
 #' @param na.rm a [logical] to indicate whether empty must be removed from `x`
 #' @details These are the explanations of the functions:
-#' * [se()] (standard error): sd / square root of length
+#' * [se()] calculates the standard error: sd / square root of length
 #' @inheritSection math_functions Default values of `na.rm`
 #' @rdname distribution_metrics
 #' @name distribution_metrics
 #' @export
 se <- function(x, na.rm = getOption("na.rm", FALSE)) {
-  if (na.rm == TRUE) {
+  if (isTRUE(na.rm)) {
     n <- length(stats::na.omit(x))
   } else {
     n <- length(x)
@@ -38,8 +38,22 @@ se <- function(x, na.rm = getOption("na.rm", FALSE)) {
 }
 
 #' @rdname distribution_metrics
+#' @param level alpha level, defaults to 95%
+#' @details * [ci()] calculates the confidence intervals for a mean (defaults at 95%), which returns length 2 
+#' @export
+ci <- function(x, level = 0.95, na.rm = getOption("na.rm", FALSE)) {
+  if (isTRUE(na.rm)) {
+    n <- length(stats::na.omit(x))
+  } else {
+    n <- length(x)
+  }
+  out <- stats::qt(p = 1 - (1 - 0.95) / 2, df = n - 1) * se(x, na.rm = na.rm)
+  mean(x, na.rm = na.rm) + c(- out, out)
+}
+
+#' @rdname distribution_metrics
 #' @param correct_mean with `TRUE` (the default) correct for the mean will be applied, by summing each square of `x` after the mean of `x` has been subtracted, so that this says something about `x`. With `FALSE`, all `x^2` are simply added together, so this says something about `x`'s location in the data.
-#' @details * [sum_of_squares()]: sum of (x - mean(x)) ^ 2
+#' @details * [sum_of_squares()] calculates the sum of (x - mean(x)) ^ 2
 #' @seealso For the sum of squares: <https://www.thoughtco.com/sum-of-squares-formula-shortcut-3126266>
 #' @export
 sum_of_squares <- function(x, correct_mean = TRUE, na.rm = getOption("na.rm", FALSE)) {
@@ -51,14 +65,14 @@ sum_of_squares <- function(x, correct_mean = TRUE, na.rm = getOption("na.rm", FA
 }
 
 #' @rdname distribution_metrics
-#' @details * [cv()] (coefficient of variation): standard deviation / mean
+#' @details * [cv()] calculates the coefficient of variation: standard deviation / mean
 #' @export
 cv <- function(x, na.rm = getOption("na.rm", FALSE)) {
   sd(x, na.rm = na.rm) / abs(mean(x, na.rm = na.rm))
 }
 
 #' @rdname distribution_metrics
-#' @details * [cqv()] (coefficient of quartile variation): (Q3 - Q1) / (Q3 + Q1)
+#' @details * [cqv()] calculates the coefficient of quartile variation: (Q3 - Q1) / (Q3 + Q1)
 #' @export
 cqv <- function(x, na.rm = getOption("na.rm", FALSE)) {
   cqv.x <-
@@ -68,21 +82,21 @@ cqv <- function(x, na.rm = getOption("na.rm", FALSE)) {
 }
 
 #' @rdname distribution_metrics
-#' @details * [z_score()] (number of standard deviations from the mean): (x - mean) / sd
+#' @details * [z_score()] calculates the number of standard deviations from the mean: (x - mean) / sd
 #' @export
 z_score <- function(x, na.rm = getOption("na.rm", FALSE)) {
   (x - mean(x, na.rm = na.rm)) / sd(x, na.rm = na.rm)
 }
 
 #' @rdname distribution_metrics
-#' @details * [midhinge()] (mean of interquartile range): (Q1 + Q3) / 2
+#' @details * [midhinge()] calculates the mean of interquartile range: (Q1 + Q3) / 2
 #' @export
 midhinge <- function(x, na.rm = getOption("na.rm", FALSE)) {
   unname((quantile(x, 0.25, na.rm = na.rm) + quantile(x, 0.75, na.rm = na.rm)) / 2)
 }
 
 #' @rdname distribution_metrics
-#' @details * [ewma()] (EWMA, exponentially weighted moving average)
+#' @details * [ewma()] calculates the EWMA (exponentially weighted moving average)
 #' @param lambda smoothing parameter, a value between 0 and 1. A value of 0 is equal to `x`, a value of 1 equal to the *mean* of `x`. The EWMA looks back and has a delay - the rrEWMA takes the mean of a 'forward' and 'backward' EWMA.
 #' @export
 ewma <- function(x, lambda, na.rm = getOption("na.rm", FALSE)) {
@@ -96,7 +110,7 @@ ewma <- function(x, lambda, na.rm = getOption("na.rm", FALSE)) {
 
 
 #' @rdname distribution_metrics
-#' @details * [rr_ewma()] (rrEWMA, reversed-recombined exponentially weighted moving average)
+#' @details * [rr_ewma()] calculates the rrEWMA (reversed-recombined exponentially weighted moving average)
 #' @export
 rr_ewma <- function(x, lambda, na.rm = getOption("na.rm", FALSE)) {
   # Reversed-Recombined EWMA
@@ -113,11 +127,26 @@ rr_ewma <- function(x, lambda, na.rm = getOption("na.rm", FALSE)) {
 #' @param n number to be normalised
 #' @param n_ref reference to use for normalisation
 #' @param per normalisation factor
+#' @details * [normalise()] normalises the data based on a reference: (n / reference) * unit
 #' @export
-normalise <- function (n, n_ref, per = 1000) {
+normalise <- function(n, n_ref, per = 1000) {
   (n / n_ref) * per
 }
 
 #' @rdname distribution_metrics
 #' @export
 normalize <- normalise
+
+#' @rdname distribution_metrics
+#' @details * [scale_sd()] normalises the data to have a standard deviation of 1, while retaining the mean
+#' @export
+scale_sd <- function(x) {
+  z_score(x, na.rm = TRUE) + mean(x, na.rm = TRUE)
+}
+
+#' @rdname distribution_metrics
+#' @details * [centre_mean()] normalises the data to have a mean of 0, while retaining the standard deviation
+#' @export
+centre_mean <- function(x) {
+  x - mean(x, na.rm = TRUE)
+}
